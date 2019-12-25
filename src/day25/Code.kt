d1a5bfbbc85716d0a331792b59cdd75389cf379f
Francesco Colasuonno <fcolasuonno@gmail.com>
2019-12-25 08:10:59 +0000

    Day 25

--- src/day25/Code.kt
+++ src/day25/Code.kt
@@ -42,6 +42,86 @@ fun part1(input: List<Long>) {
      *  fuel cell
      *  tambourine
      *  mutex
+     *
+     *  maze:
+     *
+    Hull Breach
+    N-Corridor
+    E-Storage
+    W-Sick Bay
+
+    Sick Bay
+    E-Hull Breach
+    W-Hot Chocolate Fountain
+
+    Hot Chocolate Fountain
+    N-Warp Drive Maintenance
+    E-Sick Bay
+    W-Crew Quarters
+
+    Crew Quarters
+    E-Hot Chocolate Fountain
+
+    Warp Drive Maintenance
+    S-Hot Chocolate Fountain
+
+    Corridor                cake
+    N-Stables
+    E-Passages
+    S-Hull Breach
+
+    Passages
+    N-Gift Wrapping Center
+    E-Arcade
+    W-Corridor
+
+    Arcade
+    N-Navigation
+    W-Passages
+
+    Navigation      pointer
+    S-Arcade
+
+    Gift Wrapping Center
+    S-Passages
+
+    Stables            mutex
+    E-Engineering
+    S-Corridor
+    W-Kitchen
+
+    Engineering         antenna
+    W-Stables
+
+    Kitchen
+    E-Stables
+
+    Storage
+    W-Hull Breach
+    E-Observatory
+
+    Observatory        tambourine
+    N-Science Lab
+    E-Holodeck
+    W-Storage
+
+    Science Lab
+    S-Observatory
+
+    Holodeck     fuel cell
+    E-Hallway
+    W-Observatory
+
+    Hallway       boulder
+    N-Security Checkpoint
+    W-88
+
+    Security Checkpoint
+    E-Pressure-Sensitive Floor
+    S-Hallway
+
+    Pressure-Sensitive Floor
+    W-Security Checkpoint
      */
     while (!inputCommand.joinToString("").startsWith("quit")) {
         comp.runWhile { peekOp is IntCode.Input && inputCommand.isEmpty() }
